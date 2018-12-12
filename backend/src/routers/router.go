0af60dc1050d3c0c3748d6ea63e9ac6407851a24
dos <noblesse721@gmail.com>
2018-12-12 18:01:30 +0900

    - 템플릿찾는중

--- backend/src/routers/router.go
+++ backend/src/routers/router.go
@@ -1,21 +1,22 @@
 package routers
 
 import (
-	"controllers"
+	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
 
 func InitRouter(r *gin.Engine) {
-	r.GET("/", func(c *gin.Context) {
-		c.JSON(200, gin.H{"name": "dos"})
+	r.LoadHTMLGlob("views/*")
+	r.GET("/*", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "main.tmpl", gin.H{})
 	})
-
-	dlogApi := r.Group("/dlog")
-	{
-		dlogApi.GET("/me", controllers.DlogMe)
-	}
-
+	/*
+		dlogApi := r.Group("/dlog")
+		{
+			dlogApi.GET("/me", controllers.DlogMe)
+		}
+	*/
 	r.NoRoute(func(c *gin.Context) {
 		c.JSON(404, gin.H{"name": "NotFound"})
 	})
