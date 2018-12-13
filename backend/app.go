20d6b99890100811051bfc3dc741fd7a24385822
dos <noblesse721@gmail.com>
2018-12-13 17:56:06 +0900

    - 폴더명 변경

--- backend/app.go
+++ backend/app.go
@@ -3,14 +3,14 @@ package main
 import (
 	"log"
 
-	r "github.com/dosReady/dlog/backend/router"
+	r "github.com/dosReady/dlog/backend/routers"
 
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	router := gin.Default()
-	router.LoadHTMLGlob("templates/*")
+	router.LoadHTMLGlob("template/*")
 	r.SettingRouters(router)
 	log.Fatal(router.Run(":80"))
 }
